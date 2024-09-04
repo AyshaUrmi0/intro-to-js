0f9ca6e068856ae38c90289bc20fab41471ef55f
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-09-05 07:36:39 +0900

    task1 added

---
+++ task1.js
@@ -0,0 +1,12 @@
+
+
+
+// Task: You have 2000 taka. You bought 700 taka oranges and 1200 taka apples. How much taka you have now?
+
+let taka = 2000;
+let oranges = 700;
+let apples = 1200;
+
+let result = taka - (oranges + apples);
+
+console.log(result);
